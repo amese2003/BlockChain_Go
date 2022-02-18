153a1fe30ee8c061fdd42c2b3aef3b7fe4777a55
Yong Ju Kwak <amese1225@gmail.com>
2022-02-18 11:01:54 +0900

    첫 체인

--- Block/main.go
+++ Block/main.go
@@ -11,10 +11,40 @@ type block struct {
 	prevHash string
 }
 
+type BlockChain struct {
+	blocks []block
+}
+
+func (b *BlockChain) GetLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+
+	return ""
+}
+
+func (b *BlockChain) AddBlock(data string) {
+	newBlock := block{data, "", b.GetLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *BlockChain) ListBlock() {
+
+	for _, block := range b.blocks {
+		fmt.Printf("Data : %s\n", block.data)
+		fmt.Printf("Hash : %s\n", block.hash)
+		fmt.Printf("PrevHash : %s\n\n", block.prevHash)
+	}
+
+}
+
 func main() {
-	testblock := block{"test block", "", ""}
-	hash := sha256.Sum256([]byte(testblock.data + testblock.prevHash))
-	hexHash := fmt.Sprintf("%x", hash)
-	testblock.hash = hexHash
-	fmt.Println(testblock)
+	var testchain BlockChain
+	testchain.AddBlock("genesis block")
+	testchain.AddBlock("a block")
+	testchain.AddBlock("b block")
+
+	testchain.ListBlock()
 }
