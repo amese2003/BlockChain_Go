0d0e973b5d0fd9cd2ce0a33f619754e12fabed06
Yong Ju Kwak <amese1225@gmail.com>
2022-02-23 23:03:27 +0900

    json 테스트

--- Block/main.go
+++ Block/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"blockchain/explorer"
+	utils "blockchain/Utils"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "테스트 JSON",
+		},
+	}
+
+	b, err := json.Marshal(data)
+	utils.HandleError(err)
+
+	fmt.Printf("%s\n", b)
+}
+
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
