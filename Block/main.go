b1ea2f1a8a2d7db52f69c2cbc09983aab87d1e62
Yong Ju Kwak <amese1225@gmail.com>
2022-03-02 23:57:23 +0900

    Parsing Commands

--- Block/main.go
+++ Block/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	rest "blockchain/Rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("테스트 코인입니다!\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer:	Start the HTML Explorer\n")
+	fmt.Printf("rest:		Start the REST API (추천!))\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
+
 }
