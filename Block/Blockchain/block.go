e994708072d41d75d8cd99b67020601161a6f9c2
Yong Ju Kwak <amese1225@gmail.com>
2022-03-06 17:25:59 +0900

    채굴

--- Block/Blockchain/block.go
+++ Block/Blockchain/block.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 )
 
 const difficulty int = 2
@@ -29,6 +30,23 @@ func (b *Block) restore(data []byte) {
 	utils.FromBytes(b, data)
 }
 
+func (b *Block) Mine() {
+	target := strings.Repeat("0", b.Difficulty)
+
+	for {
+		blockAsString := fmt.Sprint(b)
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
+		fmt.Printf("Block as String:%s\nHash:%s\nTarget:%s\nNonce:%d\n\n\n", blockAsString, hash, target, b.Nonce)
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+
+}
+
 func FindBlock(hash string) (*Block, error) {
 	blockbyte := db.Block(hash)
 
@@ -43,14 +61,15 @@ func FindBlock(hash string) (*Block, error) {
 
 func createBlock(data string, prevHash string, height int) *Block {
 	newBlock := &Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: prevHash,
-		Height:   height,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: difficulty,
+		Nonce:      0,
 	}
 
-	payload := newBlock.Data + newBlock.PrevHash + fmt.Sprint(newBlock.Height)
-	newBlock.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	newBlock.Mine()
 	newBlock.persist()
 	return newBlock
 }
--- Block/main.go
+++ Block/main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"blockchain/cli"
+	"blockchain/db"
 )
 
 func main() {
-	difficulty := 4
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("merong"+fmt.Sprint(nonce))))
-		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
-
-		if strings.HasPrefix(hash, target) {
-			return
-		} else {
-			nonce++
-		}
-	}
-
+	defer db.Close()
+	cli.Start()
 }
