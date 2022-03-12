262608b8a1e07ce015d3f8b95e4cbdfcc609714c
용주 곽 <amese1225@gmail.com>
2022-03-12 16:36:18 +0900

    월렛 저장

--- Block/wallet/wallet.go
+++ Block/wallet/wallet.go
@@ -1,7 +1,11 @@
 package wallet
 
 import (
+	utils "blockchain/Utils"
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
 	"os"
 )
 
@@ -11,15 +15,37 @@ type wallet struct {
 
 var w *wallet
 
+const (
+	fileName string = "nerocoin.wallet"
+)
+
 func hasWalletFile() bool {
-	_, err := os.Stat("nerocoin.wallet")
+	_, err := os.Stat(fileName)
 	return !os.IsNotExist(err)
+}
+
+func createPrivateKey() *ecdsa.PrivateKey {
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	utils.HandleError(err)
+	return privKey
+}
+
+func saveKey(key *ecdsa.PrivateKey) {
+	bytes, err := x509.MarshalECPrivateKey(key)
+	utils.HandleError(err)
+	err = os.WriteFile(fileName, bytes, 0644)
+	utils.HandleError(err)
 }
 
 func Wallet() *wallet {
 	if w == nil {
+		w = &wallet{}
 		if hasWalletFile() {
 
+		} else {
+			key := createPrivateKey()
+			saveKey(key)
+			w.privateKey = key
 		}
 	}
 
