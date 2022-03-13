84e0561157d85661a06140dec5c0e1e81290dcb0
용주 곽 <amese1225@gmail.com>
2022-03-13 19:04:36 +0900

    read, receive and close

--- Block/main.go
+++ Block/main.go
@@ -5,16 +5,31 @@ import (
 	"time"
 )
 
-func countToTen(name string) {
+func countToTen(c chan<- int) {
 	for i := range [10]int{} {
-		fmt.Println(i, name)
 		time.Sleep(1 * time.Second)
+		fmt.Printf("sending %d\n", i)
+		c <- i
+	}
+
+	close(c)
+}
+
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if ok == false {
+			fmt.Println("끝.")
+			break
+		}
+
+		fmt.Printf("뭘 받았나? : %d\n", a)
 	}
 }
 
 func main() {
-	go countToTen("first")
-	go countToTen("second")
-	for {
-	}
+	c := make(chan int)
+	go countToTen(c)
+	receive(c)
+
 }
