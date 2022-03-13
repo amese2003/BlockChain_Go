ff6330fbaced04abac120c2725549abe4757c52d
용주 곽 <amese1225@gmail.com>
2022-03-13 17:48:22 +0900

    go 루틴

--- Block/main.go
+++ Block/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"blockchain/cli"
-	"blockchain/db"
+	"fmt"
+	"time"
 )
 
+func countToTen(name string) {
+	for i := range [10]int{} {
+		fmt.Println(i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	defer db.Close()
-	cli.Start()
+	go countToTen("first")
+	go countToTen("second")
+	for {
+	}
 }
