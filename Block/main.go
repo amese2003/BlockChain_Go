de5aec411ae55b434c1589642917d1e79e686832
용주 곽 <amese1225@gmail.com>
2022-03-21 21:16:35 +0900

    Coverage

--- Block/main.go
+++ Block/main.go
@@ -6,6 +6,9 @@ import (
 )
 
 // 블록체인 프로그램을 시작합니다.
+// 테스트의 경우 go test -v -coverprofile "cover.out" ./... 를 사용하세요.
+// 추적된 테스트는 go tool cover -html="cover.out"
+// godoc -http:6060
 func main() {
 	defer db.Close()
 	cli.Start()
